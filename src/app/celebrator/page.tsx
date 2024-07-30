60d581919c39280d6f7a3b3194683e56bda90553
Andrew Weinrich <asweinrich@gmail.com>
2024-07-30 15:32:48 -0700

    Lots of Updates
    Adding in generator pages which means adding a lot of files and fine tuning/updating previous version code

---
+++ src/app/celebrator/page.tsx
@@ -0,0 +1,80 @@
+'use client' 
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Celebrator = () => {
+  const [nftID, setNftID] = useState('');
+  const [nftImg, setNftImg] = useState('');
+  const [attributes, setAttributes] = useState([]);
+  const [error, setError] = useState('');
+
+  const handleFetchAttributes = async () => {
+
+    const contractAddress = '0xe9A1a323b4c8FD5Ce6842edaa0cd8af943cBdf22';
+    const apiUrl = `https://api.opensea.io/api/v2/chain/ethereum/contract/${contractAddress}/nfts/${nftID}`;
+
+    try {
+      const response = await axios.get(apiUrl, {
+        params: {
+
+        }, 
+        headers: {
+          'Accept': 'application/json',
+          'X-API-KEY': 'bd019ee388f74005b6eb1f5f3d184523'
+        }
+      });
+      if (response.status === 200) {
+        console.log(response.data)
+        setAttributes(response.data.nft.traits);
+        setNftImg(`https://api.thewildbunch.io/wildbunch/${nftID}.png`)
+        setError('');
+      } else {
+        setError('Failed to fetch data');
+        setAttributes([]);
+      }
+    } catch (err) {
+      setError('Failed to fetch data');
+      setAttributes([]);
+    }
+  };
+
+  const handleChange = (e) => {
+    setNftID(e.target.value);
+  };
+
+  return (
+    <div className="p-4 max-w-md mx-auto space-y-4">
+      <h1 className="text-2xl font-bold">NFT Attributes Fetcher</h1>
+      <input
+        type="text"
+        value={nftID}
+        onChange={handleChange}
+        placeholder="Enter NFT ID"
+        className="border rounded px-2 py-1 w-full"
+      />
+      <button
+        onClick={handleFetchAttributes}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Fetch Attributes
+      </button>
+      {error && <p className="text-red-500">{error}</p>}
+      <div>
+        <h2 className="text-xl font-semibold">Attributes:</h2>
+        <ul>
+          {attributes.map((attr, index) => (
+            <li key={index}>
+              <strong>{attr.trait_type}:</strong> {attr.value}
+            </li>
+          ))}
+        </ul>
+        <h2 className="text-xl font-semibold">Img</h2>
+        <img src={nftImg} className="w-36 h-36" />
+      </div>
+    </div>
+  );
+
+};
+
+export default Celebrator;
---
+++ src/app/malibu-high/page.tsx
@@ -0,0 +1,157 @@
+'use client' 
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const MalibuHigh = () => {
+  const [nftID, setNftID] = useState('');
+  const [nftAlias, setNftAlias] = useState('');
+  const [nftImg, setNftImg] = useState('/media/malibu-high/exampleWO.png');
+  const [attributes, setAttributes] = useState([]);
+  const [error, setError] = useState('');
+  
+
+  useEffect(() => {
+
+    const canvas = document.getElementById('canvas-preview')
+    const canvasFull = document.getElementById('canvas-full')
+    const ctx = canvas.getContext('2d')
+    const ctxFull = canvasFull.getContext('2d')
+    
+    canvasFull.width = 2388
+    canvasFull.height = 1522
+
+    canvas.width = 320
+    canvas.height = 204
+
+    const overlay = new Image()
+    overlay.src = '/media/malibu-high/malibu-high-id.svg'
+
+    const wildOneImg = new Image()
+    wildOneImg.crossOrigin = 'anonymous'
+    wildOneImg.src = nftImg 
+    wildOneImg.onload = () => {
+      ctx.drawImage(wildOneImg, (0.02*canvas.width), (0.0267*canvas.width), (0.367*canvas.width), (0.367*canvas.width))
+      //make a ratio of screen width 
+      ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height)
+      //make a ratio of screen width 
+      canvas.style.letterSpacing = '0.5px';
+      ctx.font = "22px SteezBold";
+      ctx.textAlign = "center";
+      ctx.fillStyle = '#000';
+      const paddedId = nftID.toString().padStart(4, '0')
+      ctx.fillText('ID#: '+paddedId, (0.2067*canvas.width), (0.838*canvas.height))
+      //make a ratio of screen width 
+      ctx.font = "17px SteezBold";
+      ctx.fillText(nftAlias, (0.64*canvas.width), (0.94*canvas.height))
+
+      ctxFull.drawImage(wildOneImg, 48, 64, 876, 876)
+      ctxFull.drawImage(overlay, 0, 0, 2388, 1522)
+      canvasFull.style.letterSpacing = '0.5px'
+      ctxFull.font = "175px SteezBold";
+      ctxFull.textAlign = "center";
+      ctxFull.fillStyle = '#000'
+      ctxFull.fillText('ID#: '+paddedId, 494, 1274)
+      ctxFull.font = "134px SteezBold";
+      ctxFull.textAlign = "center";
+      ctxFull.fillStyle = '#000'
+      ctxFull.fillText(nftAlias, 1528, 1416)
+    }
+  }, [nftImg]);
+
+
+  const handleFetchAttributes = async () => {
+
+    const contractAddress = '0xe9A1a323b4c8FD5Ce6842edaa0cd8af943cBdf22';
+    const apiUrl = `https://api.opensea.io/api/v2/chain/ethereum/contract/${contractAddress}/nfts/${nftID}`;
+
+    try {
+      const response = await axios.get(apiUrl, {
+        params: {
+
+        }, 
+        headers: {
+          'Accept': 'application/json',
+          'X-API-KEY': 'bd019ee388f74005b6eb1f5f3d184523'
+        }
+      });
+      if (response.status === 200) {
+        console.log(response.data)
+        setAttributes(response.data.nft.traits);
+        setNftImg(response.data.nft.display_image_url)
+        setError('');
+      } else {
+        setError('Failed to fetch data');
+        setAttributes([]);
+      }
+    } catch (err) {
+      setError('Failed to fetch data');
+      setAttributes([]);
+    }
+  };
+
+  const handleChangeId = (e) => {
+    setNftID(e.target.value);
+  };
+
+  const handleChangeAlias = (e) => {
+    setNftAlias(e.target.value);
+  };
+
+  function saveImage(element) {
+      const canvas = document.getElementById('canvas-full')
+      const image = canvas.toDataURL("image/jpg")
+      const win = window.open()
+      win.document.write('<img src="'+image+'" height="1522" width="2388" />')
+  }
+
+  return (
+    <div className="p-4 max-w-md mx-auto">
+      <h1 className="text-4xl font-bold text-center">Malibu High ID Generator</h1>
+      
+      <canvas id="canvas-preview" className="mx-auto my-4"></canvas>
+      <p className="text-xl leading-none text-center my-4">Enter your Wild One ID and alias, then hit GENERATE to get your school ID card</p>
+      <div className="space-y-3">
+        <input
+          type="number"
+          value={nftID}
+          onChange={handleChangeId}
+          placeholder="Enter Wild One ID"
+          className="border rounded px-2 py-1 w-full text-black text-2xl"
+        />
+        <input
+          type="text"
+          value={nftAlias}
+          onChange={handleChangeAlias}
+          placeholder="Alias"
+          className="border rounded px-2 py-1 w-full text-black text-2xl"
+        />
+        <button
+          onClick={handleFetchAttributes}
+          className="w-full font-bold bg-red-700 text-white px-4 py-2 rounded text-3xl"
+        >
+          Generate
+        </button>
+        {error && <p className="text-red-500">{error}</p>}
+
+        <p className="text-xl leading-none text-center my-4">When your ID is ready, hit DOWNLOAD to save it in high resolution</p>
+
+        <button className="w-full font-bold bg-red-700 text-white px-4 py-2 rounded text-3xl" onClick={() => saveImage()}>Download</button>
+
+      </div>
+
+
+
+      <div className="hidden flex-column align-items-center">
+        <canvas id="canvas-full" className="canvas-full-1 mx-auto mb-4" width="2388" height="1522"></canvas>
+      </div>
+
+
+    </div>
+
+
+  );
+
+};
+
+export default MalibuHigh;

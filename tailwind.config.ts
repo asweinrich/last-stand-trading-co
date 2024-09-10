8b29ffbbff97ea4f6991d82bb76a433d5c5dca44
Andrew Weinrich <asweinrich@gmail.com>
2024-09-10 09:54:15 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ const config: Config = {
       'none': '0',
       'sm': '0.125rem',
       DEFAULT: '0.25rem',
-      DEFAULT: '4px',
       'md': '0.375rem',
       'lg': '0.5rem',
       'full': '9999px',
